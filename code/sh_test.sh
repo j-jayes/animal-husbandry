202ff43805b8e6dd340060b7e9607961841a05bf
Jonathan Jayes (Dell) <j0nathanjayes@gmail.com>
2022-09-13 17:50:03 +0200

    adding analysis

--- code/sh_test.sh
+++ code/sh_test.sh
@@ -1,2 +1,11 @@
 
 python code/tptp.py "skaraborgs/1866_66.jpg"
+
+python code/tptp.py "skaraborgs/1866_67.jpg"
+
+python code/tptp.py "skaraborgs/1866_68.jpg"
+
+python code/tptp.py skaraborgs/1866_69.jpg
+
+python code/tptp.py 'skaraborgs/1866_70.jpg'
+
